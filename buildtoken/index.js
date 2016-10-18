c113c5fec998fbd2432de83b3b383a4479c366eb
Bertus van Dalen <Bertus.vanDalen@ns.nl>
2016-10-18 08:11:53 +0200

    added comments to the function

--- buildtoken/index.js
+++ buildtoken/index.js
@@ -1,14 +1,44 @@
 module.exports = function(context, req) {
+	
+	// say hello to the logger
+	context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
     
-    var KEY_NAME = "customerCode";
-    var SECRET_KEY_VALUE = "qwertyuiop"
+	/***********\
+	|
+	| constants
+	|
+	\***********/
+	// this key identifies an embedding platform to the composition platform
+    var KEY_NAME = "starship enterprise";
+	
+	// this is a secret key that is never sent over the internet
+	// it is configured at the embedding platform server
+    var SECRET_KEY_VALUE = "qwerertrertyuytrtyuiuytrtyuiuytrtyuytrertyuiytrertyui"
     
-    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
-
-    if (req.query.userGuid || (req.body && req.body.userGuid)) {
-        var userGuid = req.query.userGuid || req.body.userGuid;
-    }
-    else {
+	/***********\
+	|
+	| variables
+	|
+	\***********/
+	// the composition platform will not accept an expiry above 48 hours
+	var keyExpiryTimeSpan = 60 * 60 * 48;
+	var expiryUnixTimeSeconds = Math.floor(Date.now() / 1000) + keyExpiryTimeSpan;
+	
+	// a random text is generated to salt the algorithm
+	var randomtext = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < 10; i++ )
+        randomtext += possible.charAt(Math.floor(Math.random() * possible.length));
+	
+    /***********\
+	|
+	| parameters
+	|
+	\***********/
+	var userGuid = "";
+    userGuid = req.query.userGuid || req.body.userGuid;
+    if(!userGuid)
+	{
         context.res = {
             status: 400,
             body: "Please pass a userGuid on the query string or in the request body"
@@ -17,20 +47,20 @@ module.exports = function(context, req) {
         return;
     }
 
-    var secondsValidityTimeSpan = 60 * 60 * 48;
-    var expiryUnixTimeSeconds = Math.floor(Date.now() / 1000) + secondsValidityTimeSpan;
-
-    var randomtext = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for( var i=0; i < 10; i++ )
-        randomtext += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    var crypto = require('crypto'); 
+    /***********\
+	|
+	| 3. 2. 1. go
+	|
+	\***********/
     var stringToSign = expiryUnixTimeSeconds + userGuid + randomtext + KEY_NAME;
-
+	var crypto = require('crypto'); 
     hash = crypto.createHmac('sha256', SECRET_KEY_VALUE).update(stringToSign).digest('hex');
     
+	/*************\
+	|
+	| return value
+	|
+	\*************/
     context.res = {
         body: {
             expiry: expiryUnixTimeSeconds,
@@ -41,5 +71,10 @@ module.exports = function(context, req) {
         }
     };
 
+	/***********\
+	|
+	| we're done
+	|
+	\***********/
     context.done();
 };
